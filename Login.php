3dc610c92798c395679791f5e6e21ab3db6d3c93
Kalin <kalinilushev.1@mail.bg>
2017-06-27 21:13:35 +0300

    color shit

--- Login.php
+++ Login.php
@@ -18,9 +18,9 @@
     <div class="container">
         <div class="row centered-form">
         <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
-        	<div class="panel panel-default">
-        		<div class="panel-heading">
-			    		<h3 class="panel-title">Please login to xSport</h3>
+        	<div class="panel panel-default" >
+        		<div class="panel-heading" >
+			    		<h3 class="panel-title" >Please login to xSport</h3>
 			 			</div>
 			 			<div class="panel-body">
 			    		<form role="form">
