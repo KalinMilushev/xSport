28dc405257de91ec2ae7cb7be128934a3f8e153b
Kalin <kalinilushev.1@mail.bg>
2017-06-28 23:07:41 +0300

    Some weird weather fidget on home page

--- Homepage.php
+++ Homepage.php
@@ -13,10 +13,8 @@
         <div class="row">
             <div class="col-sm-1"></div>
             <!-- end of col1 -->
-
-
 			<div class="container col-sm-10">
-				<div id="myCarousel" class="carousel slide" data-ride="carousel"  >
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
 					<!-- Indicators -->
 					<ol class="carousel-indicators">
 						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -58,14 +56,15 @@
 
         
 
-    </div>
-    <!-- end of containter for row1 -->
+    </div> <!-- end of containter for row1 -->
 	<br><br><br>
 	<div class="row">
 		<div class="col-sm-1"></div>
 		
 		<div class="col-sm-3 imgInsideNavBar" >
-			<img src="img/pic1.jpg"  style="width: 250px; height: 250px;">
+			<a href="https://www.accuweather.com/en/bg/sofia/51097/weather-forecast/51097" class="aw-widget-legal" ></a>
+			<div id="awcc1498680205524" class="aw-widget-current"  data-locationkey="51097" data-unit="c" data-language="en-us" data-useip="false" data-uid="awcc1498680205524" ></div>
+			<script type="text/javascript" src="https://oap.accuweather.com/launch.js"></script>
 		</div>
 		<div class="col-sm-3 imgInsideNavBar" >
 			<img src="img/pic1.jpg"  style="width: 250px; height: 250px;">
